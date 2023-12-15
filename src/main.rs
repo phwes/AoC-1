7feb9871bfd72e888cc75bc46be0ced8d099cd1e
Philip Wester <pwester@univid.io>
2023-12-15 21:14:12 +0100

    solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let sum = std::fs::read_to_string("res/input.txt")
+        .unwrap()
+        .lines()
+        .fold(0, |acc, line| {
+            let first_digit = line.chars().find(|c| c.is_digit(10)).unwrap();
+            let last_digit = line.chars().rev().find(|c| c.is_digit(10)).unwrap();
+
+            let number = first_digit.to_string() + &last_digit.to_string();
+            let number = number.parse::<i32>().unwrap();
+
+            acc + number
+        });
+    print!("{}", sum);
+    ()
 }
