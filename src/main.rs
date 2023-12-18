7db5a93407dc991a15e9c9ebf02f01d0f07b84d5
Philip Wester <pwester@univid.io>
2023-12-18 09:55:21 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::char;
 
-const FILE_PATH: &str = "res/input.txt";
+const FILE_PATH: &str = "res/test.txt";
 
 fn char_is_sign(char: char) -> bool {
     if char == '.' || char.is_digit(10) {
@@ -9,71 +9,91 @@ fn char_is_sign(char: char) -> bool {
     return true;
 }
 
-fn has_adjacent_sign(matrix: &Vec<String>, i: usize, j: usize) -> bool {
-    let mut has_adjacent_sign = false;
-    let mut adjacent_signs = 0;
+fn find_start_at_number(matrix: &Vec<String>, i: usize, j: usize) -> usize {
+    println!("char: {}", matrix[i].chars().nth(j).unwrap());
+    let mut current_j = j;
+    if !matrix[i].chars().nth(current_j).unwrap().is_digit(10) {
+        panic!("No number at this position");
+    }
+
+    while current_j > 0 {
+        let char = matrix[i].chars().nth(current_j - 1).unwrap();
+        if char_is_sign(char) {
+            current_j += 1;
+            break;
+        }
+        current_j -= 1;
+    }
+
+    return current_j;
+}
+
+fn get_number_at(matrix: &Vec<String>, i: usize, j: usize) -> i32 {
+    let mut number = String::new();
+    let mut current_char = matrix[i].chars().nth(j).unwrap();
+    let mut current_j = find_start_at_number(matrix, i, j);
+
+    while current_char.is_digit(10) {
+        number.push(current_char);
+
+        if current_j == matrix[i].len() - 1 {
+            break;
+        }
+
+        current_j += 1;
+        current_char = matrix[i].chars().nth(current_j).unwrap();
+    }
+
+    number.parse::<i32>().unwrap()
+}
+
+fn get_adjacent_numbers_sum(matrix: &Vec<String>, i: usize, j: usize) -> i32 {
+    let mut adjacent_numbers = 0;
+    let mut product = 1;
 
     if i > 0 {
-        if j > 0 {
-            if char_is_sign(matrix[i - 1].chars().nth(j - 1).unwrap()) {
-                adjacent_signs += 1;
-            }
-        }
-        if char_is_sign(matrix[i - 1].chars().nth(j).unwrap()) {
-            adjacent_signs += 1;
-        }
-        if j < matrix[i].len() - 1 {
-            if char_is_sign(matrix[i - 1].chars().nth(j + 1).unwrap()) {
-                adjacent_signs += 1;
-            }
-        }
-    }
-
-    if j > 0 {
-        if char_is_sign(matrix[i].chars().nth(j - 1).unwrap()) {
-            adjacent_signs += 1;
-        }
-    }
-    if j < matrix[i].len() - 1 {
-        if char_is_sign(matrix[i].chars().nth(j + 1).unwrap()) {
-            adjacent_signs += 1;
+        let char = matrix[i - 1].chars().nth(j).unwrap();
+        if char.is_digit(10) {
+            adjacent_numbers += 1;
+            product *= get_number_at(matrix, i - 1, j)
         }
     }
 
     if i < matrix.len() - 1 {
-        if j > 0 {
-            if char_is_sign(matrix[i + 1].chars().nth(j - 1).unwrap()) {
-                adjacent_signs += 1;
+        let char = matrix[i + 1].chars().nth(j).unwrap();
+        if char.is_digit(10) {
+            adjacent_numbers += 1;
+            product *= get_number_at(matrix, i + 1, j)
+        }
+    }
+
+    if j > 0 {
+        let char = matrix[i].chars().nth(j - 1).unwrap();
+        if char.is_digit(10) {
+            adjacent_numbers += 1;
+            product *= get_number_at(matrix, i, j - 1)
+        } else if i > 0 {
+            let char = matrix[i - 1].chars().nth(j - 1).unwrap();
+            if char.is_digit(10) {
+                adjacent_numbers += 1;
+                product *= get_number_at(matrix, i - 1, j - 1)
             }
         }
-        if char_is_sign(matrix[i + 1].chars().nth(j).unwrap()) {
-            adjacent_signs += 1;
+    }
+
+    if j < matrix[i].len() - 1 {
+        let char = matrix[i].chars().nth(j + 1).unwrap();
+        if char.is_digit(10) {
+            adjacent_numbers += 1;
+            product *= get_number_at(matrix, i, j + 1)
         }
-        if j < matrix[i].len() - 1 {
-            if char_is_sign(matrix[i + 1].chars().nth(j + 1).unwrap()) {
-                adjacent_signs += 1;
-            }
-        }
+    }
+    print!("Adjacent numbers: {}\n", adjacent_numbers);
+    if adjacent_numbers < 2 {
+        return 0;
     }
 
-    if adjacent_signs > 0 {
-        has_adjacent_sign = true;
-    }
-
-    return has_adjacent_sign;
-}
-
-fn is_last_digit_in_number(matrix: &Vec<String>, i: usize, j: usize) -> bool {
-    if matrix[i].len() == j + 1 {
-        return true;
-    }
-
-    let next_char = matrix[i].chars().nth(j + 1).unwrap();
-    if char_is_sign(next_char) || next_char == '.' {
-        return true;
-    }
-
-    return false;
+    return product; // Calculate adjacent numbers
 }
 
 fn main() {
@@ -91,25 +111,10 @@ fn main() {
         for j in 0..matrix[i].len() {
             let char = matrix[i].chars().nth(j).unwrap();
 
-            if char_is_sign(char) || char == '.' {
+            if char != '*' {
                 continue;
             }
-
-            if char.is_digit(10) {
-                current_number.push(char);
-            }
-
-            if has_adjacent_sign(&matrix, i, j) {
-                add_number_to_sum = true;
-            }
-
-            if is_last_digit_in_number(&matrix, i, j) {
-                if add_number_to_sum {
-                    sum += current_number.parse::<i32>().unwrap();
-                    add_number_to_sum = false;
-                }
-                current_number = String::new();
-            }
+            sum += get_adjacent_numbers_sum(&matrix, i, j);
         }
     }
     print!("Sum: {}", sum)
